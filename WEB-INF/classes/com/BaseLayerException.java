3ffac6b5984e2a8ae4c875fcb7c0ba290f81dae7
LjNick <lj.nick@gmail.com>
2015-03-22 21:17:27 -0600

    Created a base layer to handle interactions with the database for page layers (it executes the sql statements)

---
+++ WEB-INF/classes/com/BaseLayerException.java
@@ -0,0 +1,22 @@
+package com;
+import java.lang.*;
+
+class BaseLayerException extends Exception
+{
+	public BaseLayerException(){}
+	
+	public BaseLayerException(String message)
+	{
+		super(message);
+	}
+	
+	public BaseLayerException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+	
+	public BaseLayerException(Throwable cause)
+	{
+		super(cause);
+	}
+}
