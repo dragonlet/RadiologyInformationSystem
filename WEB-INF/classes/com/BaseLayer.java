a2d2a55ff1dc5f54907e92c79207d1d108afd5a6
IDrinkTooMuchCoffee <risticpe@ualberta.ca>
2015-03-25 14:05:09 -0600

    Update BaseLayer.java
    Modified so that it opening and closing connection is done using a method; other modules may subclass the baselayer for simplicity.

--- WEB-INF/classes/com/BaseLayer.java
+++ WEB-INF/classes/com/BaseLayer.java
@@ -2,38 +2,63 @@ package com;
 import java.lang.*;
 import java.io.*;
 import java.sql.*;
-import com.BaseLayerException;
 
-public class BaseLayer {
-	private final String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
-	private final String DB_CONN_STRING = "jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
-	private final String ORA_USERNAME = "USERNAME";
-	private final String ORA_PASSWORD = "PASSWORD";
+public class BaseLayer{
+    /* Any class extending the base layer should call open connection vefore executing
+       queries using GetQueryResult(). Once finished, closeConnection should be called. */
+
+
+    private final String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
+    private final String DB_CONN_STRING = "jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
+    private final String ORA_USERNAME = "risticpe";
+    private final String ORA_PASSWORD = "compsci1";
+    private Connection conn;
 	
-	public BaseLayer(){}
+    public BaseLayer(){}
+
+    public void openConnection() throws BaseLayerException
+    {
+	try
+	    {
+		//load and register the driver
+		Class drvClass = Class.forName(DRIVER_NAME); 
+		DriverManager.registerDriver((Driver) drvClass.newInstance());
+
+		//establish the connection 
+		conn = DriverManager.getConnection(DB_CONN_STRING,ORA_USERNAME,ORA_PASSWORD); /* Oracle login info here */
+		conn.setAutoCommit(false);
+	    }
+	catch(Exception ex)
+	    {
+		throw new BaseLayerException("Error in querying DB: " + ex.getMessage(), ex);
+	    }
+    }
+
+    public void closeConnection() throws BaseLayerException
+    {
+	try
+	    {
+		conn.close();
+	    }
+	catch(Exception ex)
+	    {
+		throw new BaseLayerException("Error in querying DB: " + ex.getMessage(), ex);
+	    }
+    }
 	
-	public ResultSet GetQueryResult(String sql) throws BaseLayerException
-	{
-		try
-		{
-			//load and register the driver
-			Class drvClass = Class.forName(DRIVER_NAME); 
-			DriverManager.registerDriver((Driver) drvClass.newInstance());
+    public ResultSet GetQueryResult(String sql) throws BaseLayerException
+    {
+	try
+	    {
+		/* Query the user table to determine if a vaid username/password combination has been entered. */
+		Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+		ResultSet rset = stmt.executeQuery(sql);
 
-			//establish the connection 
-			Connection conn = DriverManager.getConnection(DB_CONN_STRING,ORA_USERNAME,ORA_PASSWORD); /* Oracle login info here */
-			conn.setAutoCommit(false);
-
-			/* Query the user table to determine if a vaid username/password combination has been entered. */
-			Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-			ResultSet rset = stmt.executeQuery(sql);
-
-			conn.close();
-			return rset;
-		}
-		catch(Exception ex)
-		{
-			throw new BaseLayerException("Error in querying DB: " + ex.getMessage(), ex);
-		}
+		return rset;
+	    }
+	catch(Exception ex)
+	    {
+		throw new BaseLayerException("Error in querying DB: " + ex.getMessage(), ex);
+	    }
     }
 }
