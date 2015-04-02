8f44376bd8e5a0a0a1e2d69074291c9223f2f8ff
IDrinkTooMuchCoffee <risticpe@ualberta.ca>
2015-04-02 14:25:39 -0600

    Create initRIS.sql

---
+++ initRIS.sql
@@ -0,0 +1,2 @@
+INSERT INTO persons VALUES(10101, 'John A.', 'Macdonald', '24 Sussex Drive', 'JMac@gc.ca', '6135550294');
+INSERT INTO users VALUES('admin', 'password', 'a', 00001, TO_DATE('18150111', 'YYYYMMDD'));
