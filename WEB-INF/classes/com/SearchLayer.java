0f605b93d445dfaa2720fefb45310a4414c30665
Matt Gallagher <mrgallag@ualberta.ca>
2015-04-02 18:26:06 -0600

    Update SearchLayer.java

--- WEB-INF/classes/com/SearchLayer.java
+++ WEB-INF/classes/com/SearchLayer.java
@@ -125,7 +125,9 @@ public class SearchLayer extends BaseLayer {
       		Statement stmt;
 
 
-		ResultSet rset2 = null;
+		ResultSet rset_desc = null;
+		ResultSet rset_diag = null;
+		ResultSet rset_temp = null;
 
 		try
 	    {
@@ -141,11 +143,24 @@ public class SearchLayer extends BaseLayer {
 
 
 
-              String doSearch = "SELECT score(1), record_id, description FROM radiology_record WHERE contains(description, '"+query+"', 1) > 0 order by score(1) desc";
+             // String doSearch = "SELECT score(1), record_id, description FROM radiology_record WHERE contains(description, '"+query+"', 1) > 0 order by score(1) desc";
+              String searchDesc = "SELECT score(1), score(2), record_id, diagnosis, description FROM radiology_record WHERE (contains(description, '"+query+"', 1) > 0) OR (contains(diagnosis, '"+query+"', 2) > 0)";
+		String searchDiag = "SELECT score(1), record_id, diagnosis, description FROM radiology_record WHERE contains(diagnosis, '"+query+"', 1) > 0";
+		//String searchDesc = "SELECT score(1), record_id, diagnosis, description FROM radiology_record WHERE contains(description, '"+query+"', 1) > 0";
+
+		String tempDesc = "CREATE OR REPLACE VIEW tempDesc AS SELECT score(1) as score, record_id FROM radiology_record WHERE contains(description, '"+query+"', 1) > 0";
+
+		String tempDiag = "CREATE OR REPLACE VIEW tempDiag AS SELECT score(1) * 3 as score, record_id FROM radiology_record WHERE contains(diagnosis, '"+query+"', 1) > 0";
+
+
 
              
 		try{
-	      		rset2 = GetQueryResult(doSearch);
+	      		rset_desc = GetQueryResult(searchDesc);
+	      		rset_diag = GetQueryResult(searchDiag);
+			//Resultset rset3 = GetQueryResult("");
+			rset_temp = GetQueryResult(tempDesc);
+			rset_temp = GetQueryResult(tempDiag);
 		}
 		catch(Exception ex)
 		{
@@ -153,26 +168,37 @@ public class SearchLayer extends BaseLayer {
 		} 
 
 
+		
+
+
               toReturn = toReturn + "<table border=1>";
               toReturn = toReturn + "<tr>";
               toReturn = toReturn + "<th>Record ID</th>";
+              toReturn = toReturn + "<th>Diagnosis</th>";
               toReturn = toReturn + "<th>Description</th>";
-              toReturn = toReturn + "<th>Score</th>";
+              toReturn = toReturn + "<th>Score(1)</th>";
+              toReturn = toReturn + "<th>Score(2)</th>";
               toReturn = toReturn + "</tr>";
              
 
 	      try{
-	      while(rset2.next())
+	      while(rset_desc.next())
               {
                 toReturn = toReturn + "<tr>";
                 toReturn = toReturn + "<td>"; 
-                toReturn = toReturn + rset2.getString(2);
+                toReturn = toReturn + rset_desc.getString("record_id");
                 toReturn = toReturn + "</td>";
                 toReturn = toReturn + "<td>"; 
-                toReturn = toReturn + rset2.getString(3); 
+                toReturn = toReturn + rset_desc.getString("diagnosis"); 
+                toReturn = toReturn + "</td>";
+                toReturn = toReturn + "<td>"; 
+                toReturn = toReturn + rset_desc.getString("description"); 
                 toReturn = toReturn + "</td>";
                 toReturn = toReturn + "<td>";
-                toReturn = toReturn + rset2.getObject(1);
+                toReturn = toReturn + rset_desc.getObject("score(1)");
+                toReturn = toReturn + "</td>";
+                toReturn = toReturn + "<td>";
+                toReturn = toReturn + rset_desc.getObject("score(2)");
                 toReturn = toReturn + "</td>";
                 toReturn = toReturn + "</tr>";
               } 
