85f88d1857f7c476d177de97461f1e8108a7df45
IDrinkTooMuchCoffee <risticpe@ualberta.ca>
2015-04-02 19:41:52 -0600

    Update DataAnalysisModule.java

--- WEB-INF/classes/com/DataAnalysisModule.java
+++ WEB-INF/classes/com/DataAnalysisModule.java
@@ -8,35 +8,55 @@ public class DataAnalysisModule extends BaseLayer{
 
     public DataAnalysisModule(){}
 
-    public int countCorrespondingImages(String patient, String test_type, String time_period, String time_block)
+    public String sent_sql = "None";
+
+    public ArrayList<OlapShelf> countCorrespondingImages(String patient, String test_type, String time_block)
 	throws BaseLayerException, SQLException
     {
-	ResultSet rset = null;
-	int count = 0, qType;
+	/* time_block is one of {week, month, year}. */
 
-	if((qType = getQType(patient, test_type, time_period)) == 0)
+	ResultSet rset = null;
+	ArrayList<OlapShelf> shelvedRows = new ArrayList<OlapShelf>();
+	String year = "";
+	int count = 0, month = 0, week = 0, qType;
+
+	if((qType = getQType(patient, test_type)) == 0)
 	    {
 		throw new BaseLayerException("Error retrieving query type in DataAnalysisModule.");
 	    }
 	
 	openConnection();
 
-	String parsed_time = parseTime(time_period, time_block);
+	String time_spec = getTimeSpec(time_block);
 
 	/* Parameters should be checked and ready for appending when passed to this method. */
 
-	String sql = genSQL(qType, patient, test_type, parsed_time);
+	String sql = genSQL(qType, patient, test_type, time_block, time_spec);
+
+	sent_sql = sql;
+
 
 	rset = GetQueryResult(sql);
 
-	if(rset != null && rset.next())
+	while(rset != null && rset.next())
 	    {
-		count = Integer.parseInt((rset.getString("imgcount")).trim());
+		count = Integer.parseInt((rset.getString("Img_Count")).trim());
+
+		if(time_block.matches("month"))
+		    {
+			month = Integer.parseInt((rset.getString("month")).trim());
+		    }
+		else if(time_block.matches("week"))
+		    {
+			week = Integer.parseInt((rset.getString("week")).trim());
+		    }
+		year = rset.getString("year1");
+		shelvedRows.add(new OlapShelf(count, year, month, week));
 	    }
 
 	closeConnection();
 
-	return count;
+	return shelvedRows;
     }       
 
     public ArrayList queryTestTypes()
@@ -61,42 +81,30 @@ public class DataAnalysisModule extends BaseLayer{
 	return test_types;
     }
 
-    private String parseTime(String time_period, String time_block)
+    private String getTimeSpec(String time_block)
     {
-	String parsed_time = "";
-	
 	if(time_block.matches("week"))
 	    {
-		/* time_period will be in the format ww/yyyy */
-		String[] date_parts = time_period.split("/");
-		parsed_time = "to_number(to_char(R.test_date,'WW')) = "+date_parts[0]+" AND to_number(to_char(R.test_date,'YYYY')) = "+date_parts[1];
+		return "to_number(to_char(R.test_date,'WW'))";
 	    }
 	else if(time_block.matches("month"))
 	    {
-		/* time_period will be in the format mm/yyyy */
-		String[] date_parts = time_period.split("/");
-		parsed_time = "to_number(to_char(R.test_date,'MM')) = "+date_parts[0]+" AND to_number(to_char(R.test_date,'YYYY')) = "+date_parts[1];
+		return "to_number(to_char(R.test_date,'MM'))";
 	    }
-	else if(time_block.matches("year"))
+	else
 	    {
-		/* time_period will be in the format yyyy */
-		parsed_time = "to_number(to_char(R.test_date,'YYYY')) = "+time_period;
+		return "to_number(to_char(R.test_date,'YYYY'))";
 	    }
-	return parsed_time;
     }
 
-    private String genSQL(int type, String patient, String test_type, String time_period)
+    private String genSQL(int type, String patient, String test_type, String time_period, String time_spec)
     {
 	/* 
 	   Values of type and the corresponding queries they indicate: 
-	   1 - All patients, all times, all tests
+	   1 - All patients, all tests
 	   2 - Specific patient only
 	   3 - Specific test only
-	   4 - Specific time only
-	   5 - Specific patient and specific time
-	   6 - Specific patient and specific test
-	   7 - Specific time and specific test
-	   8 - Specific patient, time, and test
+	   4 - Specific patient and specific test
 	*/
 
 	String sql = "none";
@@ -105,42 +113,22 @@ public class DataAnalysisModule extends BaseLayer{
 	    {
 	    case 1:
 		{
-		    sql = "SELECT count(image_id) AS imgcount FROM pacs_images";
+		    sql = "SELECT to_number(to_char(R.test_date,'YYYY')) AS year1, "+time_spec+" AS "+time_period+", count(I.image_id) AS Img_Count FROM pacs_images I, radiology_record R WHERE I.record_id = R.record_id GROUP BY to_number(to_char(R.test_date,'YYYY')), "+time_spec;
 		    break;
 		}
 	    case 2:
 		{
-		    sql = "SELECT count(I.image_id) AS imgcount FROM pacs_images I, radiology_record R WHERE I.record_id = R.record_id AND R.patient_id ='"+patient+"'";
+		    sql = "SELECT to_number(to_char(R.test_date,'YYYY')) AS year1, "+time_spec+" AS "+time_period+", count(I.image_id) AS imgcount FROM pacs_images I, radiology_record R WHERE I.record_id = R.record_id AND R.patient_id ='"+patient+"' GROUP BY to_number(to_char(R.test_date,'YYYY')), "+time_spec;
 		    break;
 		}
 	    case 3:
 		{
-		    sql = "SELECT count(I.image_id) AS imgcount FROM pacs_images I, radiology_record R WHERE I.record_id = R.record_id AND R.test_type ='"+test_type+"'";
+		    sql = "SELECT to_number(to_char(R.test_date,'YYYY')) AS year1, "+time_spec+" AS "+time_period+", count(I.image_id) AS imgcount FROM pacs_images I, radiology_record R WHERE I.record_id = R.record_id AND R.test_type ='"+test_type+"' GROUP BY to_number(to_char(R.test_date,'YYYY')), "+time_spec;
 		    break;
 		}
 	    case 4:
 		{
-		    sql = "SELECT count(I.image_id) AS imgcount FROM pacs_images I, radiology_record R WHERE I.record_id = R.record_id AND "+time_period;
-		    break;
-		}
-	    case 5:
-		{
-		    sql = "SELECT count(I.image_id) AS imgcount FROM pacs_images I, radiology_record R WHERE I.record_id = R.record_id AND R.patient_id ='"+patient+"' AND "+time_period;
-		    break;
-		}
-	    case 6:
-		{
-		    sql = "SELECT count(I.image_id) AS imgcount FROM pacs_images I, radiology_record R WHERE I.record_id = R.record_id AND R.patient_id ='"+patient+"' AND R.test_type ='"+test_type+"'";
-		    break;
-		}
-	    case 7:
-		{
-		    sql = "SELECT count(I.image_id) AS imgcount FROM pacs_images I, radiology_record R WHERE I.record_id = R.record_id AND R.test_type ='"+test_type+"' AND "+time_period;
-		    break;
-		}
-	    case 8:
-		{
-		    sql = "SELECT count(I.image_id) AS imgcount FROM pacs_images I, radiology_record R WHERE I.record_id = R.record_id AND R.patient_id ='"+patient+"' AND R.test_type ='"+test_type+"' AND "+time_period;
+		    sql = "SELECT to_number(to_char(R.test_date,'YYYY')) AS year1, "+time_spec+" AS "+time_period+", count(I.image_id) AS imgcount FROM pacs_images I, radiology_record R WHERE I.record_id = R.record_id AND R.patient_id ='"+patient+"' AND R.test_type ='"+test_type+"' GROUP BY to_number(to_char(R.test_date,'YYYY')), "+time_spec;
 		    break;
 		}
 	    }
@@ -148,26 +136,18 @@ public class DataAnalysisModule extends BaseLayer{
     }
 
 
-    private int getQType(String patient, String test_type, String time_period)
+    private int getQType(String patient, String test_type)
     {
 	/* Identifying the specific set of parameter conditions. */
 
-	if(patient.matches("all") && test_type.matches("all") && time_period.matches("all"))
+	if(patient.matches("all") && test_type.matches("all"))
 	    return 1;
-	if(!patient.matches("all") && test_type.matches("all") && time_period.matches("all"))
+	if(!patient.matches("all") && test_type.matches("all"))
 	    return 2;
-	if(patient.matches("all") && !test_type.matches("all") && time_period.matches("all"))
+	if(patient.matches("all") && !test_type.matches("all"))
 	    return 3;
-	if(patient.matches("all") && test_type.matches("all") && !time_period.matches("all"))
+	if(!patient.matches("all") && !test_type.matches("all"))
 	    return 4;
-	if(!patient.matches("all") && test_type.matches("all") && !time_period.matches("all"))
-	    return 5;
-	if(!patient.matches("all") && !test_type.matches("all") && time_period.matches("all"))
-	    return 6;
-	if(patient.matches("all") && !test_type.matches("all") && !time_period.matches("all"))
-	    return 7;
-	if(!patient.matches("all") && !test_type.matches("all") && !time_period.matches("all"))
-	    return 8;
 
 	return 0;
     }
