5c7d1922cb8c110dbd52afcf4b353d38902b0267
Matt Gallagher <mrgallag@ualberta.ca>
2015-04-04 17:34:17 -0600

    Update initRIS.sql

--- initRIS.sql
+++ initRIS.sql
@@ -5,3 +5,28 @@ CREATE INDEX descriptionIndex ON radiology_record(description) INDEXTYPE IS CTXS
 CREATE INDEX diagnosisIndex ON radiology_record(diagnosis) INDEXTYPE IS CTXSYS.CONTEXT;
 CREATE INDEX fnameIndex ON persons(first_name) INDEXTYPE IS CTXSYS.CONTEXT;
 CREATE INDEX lnameIndex ON persons(last_name) INDEXTYPE IS CTXSYS.CONTEXT;
+
+
+
+set serveroutput on
+declare
+  job number;
+  job2 number;
+  job3 number;
+  job4 number;
+begin
+  dbms_job.submit(job, 'ctx_ddl.sync_index(''descriptionIndex'');',
+                  interval=>'SYSDATE+2/1440');
+  dbms_job.submit(job2, 'ctx_ddl.sync_index(''diagnosisIndex'');',
+                  interval=>'SYSDATE+2/1440');
+  dbms_job.submit(job3, 'ctx_ddl.sync_index(''fnameIndex'');',
+                  interval=>'SYSDATE+2/1440');
+  dbms_job.submit(job4, 'ctx_ddl.sync_index(''lnameIndex'');',
+                  interval=>'SYSDATE+2/1440');
+  commit;
+  dbms_output.put_line('job '||job||' has been submitted.');
+  dbms_output.put_line('job '||job2||' has been submitted.');
+  dbms_output.put_line('job '||job3||' has been submitted.');
+  dbms_output.put_line('job '||job4||' has been submitted.');
+end;
+/
