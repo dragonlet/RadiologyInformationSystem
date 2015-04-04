122679a75a6e72b10b2f058664f47878058e7a9f
IDrinkTooMuchCoffee <risticpe@ualberta.ca>
2015-04-04 12:20:26 -0600

    Update index.sql

--- Helpers/index.sql
+++ Helpers/index.sql
@@ -6,3 +6,6 @@ CREATE INDEX myindex ON radiology_record(description) INDEXTYPE IS CTXSYS.CONTEX
 CREATE INDEX diagnosisIndex ON radiology_record(diagnosis) INDEXTYPE IS CTXSYS.CONTEXT;
 
 
+SELECT DESC.record_id, DESC.score + DIAG.score + PFIRST.score + PLAST.score AS aggregate_score 
+FROM tempDesc DESC, tempDiag DIAG, ..., ... 
+WHERE DESC.record_id = DIAG.record_id = ...record_id = ....record_id;
